14931118e920fde97c8f1770da3ee1f803764f63
Bandit Silachai <mbandit001@yahoo.co.th>
2023-12-07 15:45:57 +0700

    Initial Commit

---
+++ Thai-Dictionary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Thai-Dictionary
+//
+//  Created by Bandit Silachai on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Thai-Dictionary/Thai_DictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thai_DictionaryApp.swift
+//  Thai-Dictionary
+//
+//  Created by Bandit Silachai on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Thai_DictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
